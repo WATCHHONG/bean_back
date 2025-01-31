df0440cc14235af053fac6346e22c1eb8a166366
WATCHHONG <gihong0101@gmail.com>
2025-01-31 20:13:46 +0900

    Hi, i'm back

---
+++ src/main/java/org/zerock/bean_back/BeanBackApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.bean_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanBackApplication.class, args);
+    }
+
+}
